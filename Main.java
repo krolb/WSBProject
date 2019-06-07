ca198b299616e817c8bad719bb2ff8e37d50d884
krolb <bartoszkrol97@gmail.com>
2019-06-07 20:49:54 +0200

    Update Main.java
    starting build user interface

--- Main.java
+++ Main.java
@@ -4,12 +4,42 @@ public class Main {
 //@autor Bartosz Król
     // Plik gówny main w którym będzie wykonwany słaściwy program korzystający z klas znajdujących się w innych plikach
     public static void main(String[] args) {
+        System.out.println("Witaj w programie do zarządzania kadrą pracowniczą\n");
+
+        int i=1;
+        int i2=1;
+        Scanner input = new Scanner(System.in);
+        String in;
+        while(i!=0){
+            System.out.println("Co chcesz zrobić? \n1-Wprowadź pracownika\t 2-Wyszukaj pracownika \n3-Usuń pracownika \t \t 0-Wyjdź ");
+            in = input.nextLine();
+
+            if (in.equals("0")){
+                break;
+            }
+            while (i!=0){
+
+            if (in.equals("1")) {
+                System.out.println("Wprowadzasz pracownika: \n 1-Pracownik biurowy \t 2-Sprzedawca \n 3-Dyrektor \t\t\t 0-Wyjdź ");
+                in = input.nextLine();
+                if (in.equals("1")) {
+
+                } else if (in.equals("2")) {
+
+                } else if (in.equals("3")) {
+
+                } else if (in.equals("0")) {
+                    break;
+                } else {
+                    System.out.println("podaj poprawną opcje");
+                }
+            }
 
 
-        System.out.println("Witaj w programie do zarządzania kadrą pracowniczą");
+            }
 
-        Scanner in = new Scanner(System.in);
-        in.nextLine();
-
+        }
+        System.out.printf("Koniec programu :)");
     }
+
 }
